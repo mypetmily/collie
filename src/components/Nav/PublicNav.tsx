151bf51f1286d6fe40724f723f906b56ce4fc3d8
thsoon <dlatns0201@gmail.com>
2020-12-29 01:28:49 +0900

    feat(markup): (#4) Public Layout 및 Nav 작성
    로그인, 회원가입처럼 인증 없이 사용하는 layout 규격 작성

---
+++ src/components/Nav/PublicNav.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Style from './PublicNav.scss';
+
+export function PublicNav(): JSX.Element {
+  return <div className={Style.container}></div>;
+}
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+import { PublicNav } from '../components/Nav';
+import Style from './PublicLayout.scss';
+
 interface PublicLayoutProps {
   path: string;
   component: any;
@@ -12,7 +15,8 @@ const PublicLayout = ({ path, component: Component, ...rest }: PublicLayoutProps
   return (
     <>
       {!user && (
-        <div className="container">
+        <div className={Style.container}>
+          <PublicNav />
           <Route path={path} render={({ ...routeRest }) => <Component {...rest} {...routeRest} />} />
         </div>
       )}
